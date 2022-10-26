4e0b0acf657d2af877fc4ba777377c8739f33103
Horecio Araujo Dias <horecio@gmail.com>
2022-10-26 09:56:09 -0300

    test(municipe): add basic validations

--- app/models/municipe.rb
+++ app/models/municipe.rb
@@ -1,3 +1,10 @@
 class Municipe < ApplicationRecord
-  validates :name, presence: true
+  validates :name,        presence: true
+  validates :cpf,         presence: true
+  validates :cns,         presence: true
+  validates :email,       presence: true
+  validates :birth_date,  presence: true
+  validates :phone,       presence: true
+
+  validates :email, confirmation: true
 end
--- spec/models/municipe_spec.rb
+++ spec/models/municipe_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Municipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#name' do
+    specify { is_expected.to have_db_column(:name).of_type(:string).with_options(null: false) }
+    specify { is_expected.to validate_presence_of(:name) }
+  end
+
+  describe '#cpf' do
+    specify { is_expected.to have_db_column(:cpf).of_type(:string).with_options(null: false) }
+    specify { is_expected.to validate_presence_of(:cpf) }
+  end
+
+  describe '#cns' do
+    specify { is_expected.to have_db_column(:cns).of_type(:string).with_options(null: false) }
+    specify { is_expected.to validate_presence_of(:cns) }
+  end
+
+  describe '#email' do
+    specify { is_expected.to have_db_column(:email).of_type(:string).with_options(null: false) }
+    specify { is_expected.to validate_presence_of(:email) }
+    specify { is_expected.to validate_confirmation_of(:email) }
+  end
+
+  describe '#birth_date' do
+    specify { is_expected.to have_db_column(:birth_date).of_type(:date).with_options(null: false) }
+    specify { is_expected.to validate_presence_of(:birth_date) }
+  end
+
+  describe '#phone' do
+    specify { is_expected.to have_db_column(:phone).of_type(:string).with_options(null: false) }
+    specify { is_expected.to validate_presence_of(:birth_date) }
+  end
 end
