3e3983a1bf889bad23eddbe2b0a7e0de6b441b6d
Horecio Araujo Dias <horecio@gmail.com>
2022-10-27 17:55:35 +0000

    test(municipe): add request tests

--- spec/requests/municipes_spec.rb
+++ spec/requests/municipes_spec.rb
@@ -1,119 +1,119 @@
 require 'rails_helper'
 
-RSpec.describe "/municipes", type: :request do
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  describe "GET /index" do
-    it "renders a successful response" do
-      Municipe.create! valid_attributes
+RSpec.describe '/municipes', type: :request do
+  describe 'GET /index' do
+    it 'renders a successful response' do
       get municipes_url
+
       expect(response).to be_successful
     end
   end
 
-  describe "GET /show" do
-    it "renders a successful response" do
-      municipe = Municipe.create! valid_attributes
-      get municipe_url(municipe)
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /new" do
-    it "renders a successful response" do
+  describe 'GET /new' do
+    it 'renders a successful response' do
       get new_municipe_url
+
       expect(response).to be_successful
     end
   end
 
-  describe "GET /edit" do
-    it "renders a successful response" do
-      municipe = Municipe.create! valid_attributes
+  describe 'GET /edit' do
+    it 'renders a successful response' do
+      municipe = create(:municipe)
+
       get edit_municipe_url(municipe)
+
       expect(response).to be_successful
     end
   end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Municipe" do
-        expect {
-          post municipes_url, params: { municipe: valid_attributes }
-        }.to change(Municipe, :count).by(1)
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Municipe and Endereco' do
+        expect do
+          params = attributes_for(:municipe).merge(endereco_attributes: attributes_for(:endereco))
+
+          post municipes_url, params: { municipe: params }
+        end.to change(Municipe, :count).by(1)
+           .and change(Endereco, :count).by(1)
       end
 
-      it "redirects to the created municipe" do
-        post municipes_url, params: { municipe: valid_attributes }
-        expect(response).to redirect_to(municipe_url(Municipe.last))
+      it 'redirects to the created municipe' do
+        params = attributes_for(:municipe).merge(endereco_attributes: attributes_for(:endereco))
+
+        post municipes_url, params: { municipe: params }
+
+        expect(response).to redirect_to(municipes_url)
       end
     end
 
-    context "with invalid parameters" do
-      it "does not create a new Municipe" do
-        expect {
-          post municipes_url, params: { municipe: invalid_attributes }
-        }.to change(Municipe, :count).by(0)
-      end
+    context 'when not att to params endereco attributes' do
+      it 'does not create a new Municipe and Endereco' do
+        params = attributes_for(:municipe)
 
+        expect do
+          post municipes_url, params: { municipe: params }
+        end.to change(Municipe, :count).by(0)
+           .and change(Endereco, :count).by(0)
+      end
 
       it "renders a response with 422 status (i.e. to display the 'new' template)" do
-        post municipes_url, params: { municipe: invalid_attributes }
+        params = attributes_for(:municipe)
+
+        post municipes_url, params: { municipe: params }
+
         expect(response).to have_http_status(:unprocessable_entity)
       end
-
     end
   end
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      it 'updates the requested municipe name' do
+        params = attributes_for(:municipe).merge(endereco_attributes: attributes_for(:endereco))
+        municipe = Municipe.create!(params)
 
-      it "updates the requested municipe" do
-        municipe = Municipe.create! valid_attributes
-        patch municipe_url(municipe), params: { municipe: new_attributes }
-        municipe.reload
-        skip("Add assertions for updated state")
+        new_name = 'New Name'
+        params[:name] = new_name
+        params[:endereco_attributes][:id] = municipe.endereco.id
+
+        patch municipe_url(municipe), params: { municipe: params }
+
+        expect(municipe.reload.name).to eq(new_name)
       end
 
-      it "redirects to the municipe" do
-        municipe = Municipe.create! valid_attributes
-        patch municipe_url(municipe), params: { municipe: new_attributes }
-        municipe.reload
+      it 'updates the requested endereco cep' do
+        params = attributes_for(:municipe).merge(endereco_attributes: attributes_for(:endereco))
+        municipe = Municipe.create!(params)
+
+        new_cep = '77410500'
+        params[:endereco_attributes][:cep] = new_cep
+        params[:endereco_attributes][:id] = municipe.endereco.id
+
+        patch municipe_url(municipe), params: { municipe: params }
+
+        expect(municipe.reload.endereco.cep).to eq(new_cep)
+      end
+
+      it 'redirects to the municipe' do
+        municipe = create(:municipe)
+
+        params = attributes_for(:municipe)
+        patch municipe_url(municipe), params: { municipe: params }
+
         expect(response).to redirect_to(municipe_url(municipe))
       end
     end
 
-    context "with invalid parameters" do
-
+    context 'with invalid parameters' do
       it "renders a response with 422 status (i.e. to display the 'edit' template)" do
-        municipe = Municipe.create! valid_attributes
-        patch municipe_url(municipe), params: { municipe: invalid_attributes }
+        municipe = create(:municipe)
+
+        params = attributes_for(:municipe, name: nil)
+        patch municipe_url(municipe), params: { municipe: params }
+
         expect(response).to have_http_status(:unprocessable_entity)
       end
-
-    end
-  end
-
-  describe "DELETE /destroy" do
-    it "destroys the requested municipe" do
-      municipe = Municipe.create! valid_attributes
-      expect {
-        delete municipe_url(municipe)
-      }.to change(Municipe, :count).by(-1)
-    end
-
-    it "redirects to the municipes list" do
-      municipe = Municipe.create! valid_attributes
-      delete municipe_url(municipe)
-      expect(response).to redirect_to(municipes_url)
     end
   end
 end
